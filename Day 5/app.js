4a9a63dc8397d92bf421fbc4b8f5304247764c00
Aman Mulchandani <mulchandaniaman@gmail.com>
2021-06-10 22:37:37 +0530

    Day 5 Done!

---
+++ Day 5/app.js
@@ -0,0 +1,32 @@
+"use strict";
+
+const buttons = document.querySelectorAll(".buttons");
+
+const screen = document.getElementById("display");
+
+let screenValue = "";
+let buttonText;
+
+for(const item of buttons){
+    item.addEventListener("click", (e) => {
+        buttonText = e.target.innerText;
+        if (buttonText == "RESET") {
+          screenValue = "";
+          screen.innerText = screenValue;
+        } else if (buttonText == "=") {
+          screenValue = screen.innerText;
+          screen.innerText = eval(screenValue);
+        } else if (buttonText == "DEL") {
+          
+          screenValue = screen.innerText;
+          let newData = Array.from(screenValue);
+          newData.pop();
+          let upDatedData = newData.toString().replace(/,/g, "");
+          screen.innerText = upDatedData;
+        } else {
+          screenValue = screen.innerText;
+          screenValue += buttonText;
+          screen.innerText = screenValue;
+        }
+      });
+}
