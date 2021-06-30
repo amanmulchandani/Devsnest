d79714cd727a34040ff13e7a38b6e86a82d52dc4
Aman Mulchandani <mulchandaniaman@gmail.com>
2021-06-30 12:56:37 +0530

    Day 12 Changes Done

--- Day 12/script.js
+++ Day 12/script.js
@@ -33,13 +33,17 @@ function addTodo(event)
 
     //Check Mark Button
     const completeButton = document.createElement("button");
-    completeButton.innerHTML = `<i class="fa fa-check"></i>`;
+    completeButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4M7.835 4.697a3.42 3.42 0 001.946-.806 3.42 3.42 0 014.438 0 3.42 3.42 0 001.946.806 3.42 3.42 0 013.138 3.138 3.42 3.42 0 00.806 1.946 3.42 3.42 0 010 4.438 3.42 3.42 0 00-.806 1.946 3.42 3.42 0 01-3.138 3.138 3.42 3.42 0 00-1.946.806 3.42 3.42 0 01-4.438 0 3.42 3.42 0 00-1.946-.806 3.42 3.42 0 01-3.138-3.138 3.42 3.42 0 00-.806-1.946 3.42 3.42 0 010-4.438 3.42 3.42 0 00.806-1.946 3.42 3.42 0 013.138-3.138z" />
+  </svg>`;
     completeButton.classList.add("complete-button");
     todoDiv.appendChild(completeButton);
 
     //Check Trash Button
     const trashButton = document.createElement("button");
-    trashButton.innerHTML = `<i class="fa fa-trash"></i>`;
+    trashButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+    <path fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd" />
+  </svg>`;
     trashButton.classList.add("trash-button");
     todoDiv.appendChild(trashButton);
     //Append to List
