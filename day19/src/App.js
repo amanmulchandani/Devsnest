d1a4aade730165172e0f0e705bee2601339e49f0
Aman Mulchandani <mulchandaniaman@gmail.com>
2021-07-15 13:21:54 +0530

    🎉 Day 19 Done

--- day19/src/App.js
+++ day19/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
-import Buttons from './components/Buttons/Buttons'
+
+import Box from './components/Box/Box'
 
 const App = () => {
   return (
     <div>
-      <Buttons/>
+      <Box/>
     </div>
   )
 }
---
+++ day19/src/components/Box/Box.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import { Grid, makeStyles, Container } from '@material-ui/core'
+
+import Buttons from '../Buttons/Buttons';
+const useStyles = makeStyles((theme) =>
+
+({
+    main:{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height:"100vh",
+        width:"500px"
+    },
+    paper: {
+        textAlign: 'center',
+        color: theme.palette.text.secondary,
+      },
+}));
+
+
+const Box = () => {
+    
+    const classes = useStyles();
+    return (
+        <Container className={classes.main}>
+        <Grid container className={classes.main}>
+        <Grid item xs={12} className={classes.paper}>
+            <Buttons/>
+        </Grid>
+        <Grid item xs={12} className={classes.paper}>
+            <Buttons/>
+        </Grid>
+        <Grid item xs={12} className={classes.paper}>
+            <Buttons/>
+        </Grid>
+        <Grid item xs={12} className={classes.paper}>
+            <Buttons/>
+        </Grid>
+        </Grid>
+        </Container>
+    )
+}
+
+export default Box
